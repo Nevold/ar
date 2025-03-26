7f98b0d96d45e3dd01bab1d5d39c0fab1ce2acd4
Nevold <nevold@tut.by>
2025-03-26 15:14:06 +0300

    fix

--- async-race/src/components/api-services/api.ts
+++ async-race/src/components/api-services/api.ts
@@ -1,15 +1,17 @@
 import axios from 'axios';
+
 import Constants from '../shared/constants';
+
 class Api {
-  static readonly garageApi = axios.create({
+  public static readonly garageApi = axios.create({
     baseURL: Constants.GARAGE_URL
   });
 
-  static readonly engineApi = axios.create({
+  public static readonly engineApi = axios.create({
     baseURL: Constants.ENGINE_URL
   });
 
-  static readonly winnersApi = axios.create({
+  public static readonly winnersApi = axios.create({
     baseURL: Constants.WINNERS_URL
   });
 }
--- async-race/src/components/shared/constants.ts
+++ async-race/src/components/shared/constants.ts
@@ -1,10 +1,15 @@
 class Constants {
-  static readonly FIRST_ELEM_ARRAY = 0;
-  static readonly ONE_WIN = 1;
-  static readonly API_URL = 'http://127.0.0.1:3000';
-  static readonly GARAGE_URL = `${this.API_URL}/garage`;
-  static readonly ENGINE_URL = `${this.API_URL}/engine`;
-  static readonly WINNERS_URL = `${this.API_URL}/winners`;
+  public static readonly FIRST_ELEM_ARRAY = 0;
+
+  public static readonly ONE_WIN = 1;
+
+  public static readonly API_URL = 'http://127.0.0.1:3000';
+
+  public static readonly GARAGE_URL = `${this.API_URL}/garage`;
+
+  public static readonly ENGINE_URL = `${this.API_URL}/engine`;
+
+  public static readonly WINNERS_URL = `${this.API_URL}/winners`;
 }
 
 export default Constants;
