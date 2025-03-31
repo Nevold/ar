dd7f89bcde16437999cdf43a57145c4a25b5d07c
Nevold <nevold@tut.by>
2025-03-31 15:34:21 +0300

    add

--- async-race/src/components/controllers/flipped-garage.ts
+++ async-race/src/components/controllers/flipped-garage.ts
@@ -3,22 +3,30 @@ import { updateGaragePage } from '../view/update-garage/update-garage';
 
 export function setFlippedGarage(): void {
   let numberPages = 1;
-  document.body.addEventListener('click', event => {
+  document.body.addEventListener('click', async event => {
     const element = event.target as HTMLElement;
     disabledRaceButton(element);
-    switch (true) {
-      case element.classList.contains('next-btn'):
-        numberPages++;
-        updateGaragePage(numberPages);
-        break;
 
-      case element.classList.contains('prev-btn') && numberPages > 1:
-        numberPages--;
-        updateGaragePage(numberPages);
-        break;
+    try {
+      switch (true) {
+        case element.classList.contains('next-btn'): {
+          numberPages += 1;
+          await updateGaragePage(numberPages);
+          break;
+        }
 
-      default:
-        break;
+        case element.classList.contains('prev-btn') && numberPages > 1: {
+          numberPages -= 1;
+          await updateGaragePage(numberPages);
+          break;
+        }
+
+        default: {
+          break;
+        }
+      }
+    } catch (error) {
+      console.log(`%c Error: ${String(error)}`, 'background: grey;color:#e9ed09;font-weight:bold');
     }
   });
 }
