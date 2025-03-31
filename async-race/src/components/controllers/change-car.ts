b5e499748847138caa152cc551ec31ce4972e063
Nevold <nevold@tut.by>
2025-03-31 15:20:09 +0300

    add

--- async-race/src/components/controllers/change-car.ts
+++ async-race/src/components/controllers/change-car.ts
@@ -13,58 +13,71 @@ export function changeCar(): void {
     const elementInputColor = document.querySelector('.create-color') as HTMLInputElement;
     disabledRaceButton(element);
 
-    switch (true) {
-      case element.classList.contains('remove-car'):
-        id = Number(element.id.split('remove-car-').pop());
-        await CarServices.deleteCar(id);
-        if ((await WinnersServices.getWinners()).item.filter(elem => elem.id === id).length !== 0) {
-          await WinnersServices.deleteWinnerCar(id);
+    try {
+      switch (true) {
+        case element.classList.contains('remove-car'): {
+          id = Number(element.id.split('remove-car-').pop());
+          await CarServices.deleteCar(id);
+          const winnersResult = await WinnersServices.getWinners();
+          if (winnersResult.item.some(element_ => element_.id === id)) {
+            await WinnersServices.deleteWinnerCar(id);
+          }
+          await updateGaragePage();
+          break;
         }
-        await updateGaragePage();
-        break;
 
-      case element.classList.contains('create-submit'):
-        event.preventDefault();
-        await CarServices.createCar(elementInputName.value, elementInputColor.value);
-        await updateGaragePage();
-        elementInputName.value = '';
-        elementUpdateColor.value = '#ffffff';
-        break;
+        case element.classList.contains('create-submit'): {
+          event.preventDefault();
+          await CarServices.createCar(elementInputName.value, elementInputColor.value);
+          await updateGaragePage();
+          elementInputName.value = '';
+          elementUpdateColor.value = '#ffffff';
+          break;
+        }
 
-      case element.classList.contains('select-car'):
-        id = Number(element.id.split('select-car-').pop());
-        const { name, color } = (await CarServices.getCar(id)).data;
-        document.querySelector('.update-submit')?.classList.add('update-flag');
-        elementUpdateColor.value = color;
-        elementUpdateName.value = name;
-        elementUpdateName.disabled = false;
-        elementUpdateColor.disabled = false;
-        break;
+        case element.classList.contains('select-car'): {
+          id = Number(element.id.split('select-car-').pop());
+          const carResult = await CarServices.getCar(id);
+          const { name, color } = carResult.data;
+          document.querySelector('.update-submit')?.classList.add('update-flag');
+          elementUpdateColor.value = color;
+          elementUpdateName.value = name;
+          elementUpdateName.disabled = false;
+          elementUpdateColor.disabled = false;
+          break;
+        }
 
-      case element.classList.contains('update-flag'):
-        event.preventDefault();
-        await CarServices.updateCar(id, elementUpdateName.value, elementUpdateColor.value);
-        await updateGaragePage();
-        elementUpdateName.disabled = true;
-        elementUpdateColor.disabled = true;
-        elementUpdateName.value = '';
-        elementUpdateColor.value = '#ffffff';
-        element.classList.remove('update-flag');
-        break;
+        case element.classList.contains('update-flag'): {
+          event.preventDefault();
+          await CarServices.updateCar(id, elementUpdateName.value, elementUpdateColor.value);
+          await updateGaragePage();
+          elementUpdateName.disabled = true;
+          elementUpdateColor.disabled = true;
+          elementUpdateName.value = '';
+          elementUpdateColor.value = '#ffffff';
+          element.classList.remove('update-flag');
+          break;
+        }
 
-      case element.classList.contains('update-submit'):
-        event.preventDefault();
-        break;
+        case element.classList.contains('update-submit'): {
+          event.preventDefault();
+          break;
+        }
 
-      case element.classList.contains('generate'):
-        const arrCars = generateCars();
-        const promises = arrCars.map(elem => CarServices.createCar(elem.name, elem.color));
-        await Promise.allSettled(promises);
-        await updateGaragePage();
-        break;
+        case element.classList.contains('generate'): {
+          const arrayCars = generateCars();
+          const promises = arrayCars.map(element_ => CarServices.createCar(element_.name, element_.color));
+          await Promise.allSettled(promises);
+          await updateGaragePage();
+          break;
+        }
 
-      default:
-        break;
+        default: {
+          break;
+        }
+      }
+    } catch (error) {
+      console.log(`%c Error: ${String(error)}`, 'background: grey;color:#e9ed09;font-weight:bold');
     }
   });
 }
