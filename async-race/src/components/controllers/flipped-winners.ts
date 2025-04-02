69ce28def591fe9ae8998a678fc643c03c01f2ff
Nevold <nevold@tut.by>
2025-04-02 12:36:20 +0300

    add

--- async-race/src/components/controllers/flipped-winners.ts
+++ async-race/src/components/controllers/flipped-winners.ts
@@ -2,21 +2,28 @@ import { updateWinnerPage } from '../view/winners-pages/update-winners';
 
 export function setFlippedWinners(): void {
   let numberPages = 1;
-  document.body.addEventListener('click', event => {
+  document.body.addEventListener('click', async event => {
     const element = event.target as HTMLElement;
-    switch (true) {
-      case element.classList.contains('next-btn') && element.classList.contains('winners-page'):
-        numberPages++;
-        updateWinnerPage(numberPages);
-        break;
+    try {
+      switch (true) {
+        case element.classList.contains('next-btn') && element.classList.contains('winners-page'): {
+          numberPages += 1;
+          await updateWinnerPage(numberPages);
+          break;
+        }
 
-      case element.classList.contains('prev-btn') && element.classList.contains('winners-page') && numberPages > 1:
-        numberPages--;
-        updateWinnerPage(numberPages);
-        break;
+        case element.classList.contains('prev-btn') && element.classList.contains('winners-page') && numberPages > 1: {
+          numberPages -= 1;
+          await updateWinnerPage(numberPages);
+          break;
+        }
 
-      default:
-        break;
+        default: {
+          break;
+        }
+      }
+    } catch (error) {
+      console.log(`%c Error: ${String(error)}`, 'background: grey;color:#e9ed09;font-weight:bold');
     }
   });
 }
