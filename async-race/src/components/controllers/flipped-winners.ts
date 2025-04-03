c4e64a99f6810ec9a6c64a5e544c7bb6f9a2d4de
Nevold <nevold@tut.by>
2025-04-03 16:48:11 +0300

    fix

--- async-race/src/components/controllers/flipped-winners.ts
+++ async-race/src/components/controllers/flipped-winners.ts
@@ -3,27 +3,31 @@ import { updateWinnerPage } from '../view/winners-pages/update-winners';
 export function setFlippedWinners(): void {
   let numberPages = 1;
   document.body.addEventListener('click', async event => {
-    const element = event.target as HTMLElement;
-    try {
-      switch (true) {
-        case element.classList.contains('next-btn') && element.classList.contains('winners-page'): {
-          numberPages += 1;
-          await updateWinnerPage(numberPages);
-          break;
-        }
+    if (event.target instanceof HTMLElement) {
+      const element = event.target;
+      try {
+        switch (true) {
+          case element.classList.contains('next-btn') && element.classList.contains('winners-page'): {
+            numberPages += 1;
+            await updateWinnerPage(numberPages);
+            break;
+          }
 
-        case element.classList.contains('prev-btn') && element.classList.contains('winners-page') && numberPages > 1: {
-          numberPages -= 1;
-          await updateWinnerPage(numberPages);
-          break;
-        }
+          case element.classList.contains('prev-btn') &&
+            element.classList.contains('winners-page') &&
+            numberPages > 1: {
+            numberPages -= 1;
+            await updateWinnerPage(numberPages);
+            break;
+          }
 
-        default: {
-          break;
+          default: {
+            break;
+          }
         }
+      } catch (error) {
+        console.log(`%c Error: ${String(error)}`, 'background: grey;color:#e9ed09;font-weight:bold');
       }
-    } catch (error) {
-      console.log(`%c Error: ${String(error)}`, 'background: grey;color:#e9ed09;font-weight:bold');
     }
   });
 }
