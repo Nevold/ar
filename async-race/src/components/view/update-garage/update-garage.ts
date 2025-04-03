b76ca2c07e7bc5b61758f3c15945d2b562f9637f
Nevold <nevold@tut.by>
2025-04-03 12:20:00 +0300

    add

--- async-race/src/components/view/update-garage/update-garage.ts
+++ async-race/src/components/view/update-garage/update-garage.ts
@@ -3,11 +3,17 @@ import CarServices from '../../api-services/car-services';
 
 export async function updateGaragePage(numberPages = 1): Promise<void> {
   const rootGarage = document.querySelector('#garage');
-  const carsList = await CarServices.getCars(numberPages);
-  if (carsList) {
-    const { item, count } = carsList;
-    if (rootGarage) {
-      rootGarage.innerHTML = renderGarage(item, count, numberPages);
+  try {
+    const carsList = await CarServices.getCars(numberPages);
+    if (carsList) {
+      const { item, count } = carsList;
+      if (rootGarage) {
+        // rootGarage.innerHTML = renderGarage(item, count, numberPages);
+        rootGarage.replaceChildren();
+        rootGarage.insertAdjacentHTML('afterbegin', renderGarage(item, count, numberPages));
+      }
     }
+  } catch (error) {
+    console.log(`%c Error: ${String(error)}`, 'background: grey;color:#e9ed09;font-weight:bold');
   }
 }
