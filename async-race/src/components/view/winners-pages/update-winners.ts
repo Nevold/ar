5f3ba1caa679db3ffcd5aa134bf539df11af9e2e
Nevold <Nevold@tut.by>
2025-04-03 22:02:41 +0300

    add

--- async-race/src/components/view/winners-pages/update-winners.ts
+++ async-race/src/components/view/winners-pages/update-winners.ts
@@ -1,8 +1,18 @@
 import { renderWinners } from './winner';
 import WinnersServices from '../../api-services/winners-services';
+import { Nodes } from '../nodes';
 
 export async function updateWinnerPage(numberPages = 1, limit = 10, sort = 'id', order = 'ASC'): Promise<void> {
-  const rootWinner = document.getElementById('winner');
-  const { item, count } = await WinnersServices.getWinners(numberPages, limit, sort, order);
-  if (rootWinner) rootWinner.innerHTML = await renderWinners(item, count, numberPages);
+  const rootWinner = Nodes.winnerPage;
+  try {
+    const { item, count } = await WinnersServices.getWinners(numberPages, limit, sort, order);
+
+    if (rootWinner) {
+      rootWinner.replaceChildren();
+      const innerHtml = await renderWinners(item, count, numberPages);
+      rootWinner.insertAdjacentHTML('afterbegin', innerHtml);
+    }
+  } catch (error) {
+    console.log(`%c Error: ${String(error)}`, 'background: grey;color:#e9ed09;font-weight:bold');
+  }
 }
